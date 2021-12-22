e7565d0d2d4726e63d6e3e97efee90544f4a6283
Evgenii_Efimchik <evgenii_efimchik@epam.com>
2021-12-22 12:38:03 +0300

    Initial commit

---
+++ src/main/java/com/epam/rd/autotasks/GraduallyDecreasingCarousel.java
@@ -0,0 +1,7 @@
+package com.epam.rd.autotasks;
+
+public class GraduallyDecreasingCarousel extends DecrementingCarousel{
+    public GraduallyDecreasingCarousel(final int capacity) {
+        super(capacity);
+    }
+}
